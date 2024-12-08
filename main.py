a36a31759d1e04c3017ea3a22e1b5d52ffb57114
Alla <allasamoilenko@gmx.de>
2024-12-08 17:38:03 +0200

    task3

--- main.py
+++ main.py
@@ -93,5 +93,94 @@ print(tree.search_book("Дракула"))
 print(tree.get_books_by_genre("Класика"))
 print(tree.get_books_by_genre("Хоррор"))
 
+#task3
+class FileNode:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self.left = None
+        self.right = None
 
+class FileTree:
+    def __init__(self):
+        self.root = None
 
+    def add_file(self, name, size):
+        new_file = FileNode(name, size)
+        if self.root is None:
+            self.root = new_file
+        else:
+            self._add(self.root, new_file)
+
+    def _add(self, current_node, new_file):
+        if new_file.name < current_node.name:
+            if current_node.left is None:
+                current_node.left = new_file
+            else:
+                self._add(current_node.left, new_file)
+        elif new_file.name > current_node.name:
+            if current_node.right is None:
+                current_node.right = new_file
+            else:
+                self._add(current_node.right, new_file)
+
+    def find_file(self, name):
+        return self._find(self.root, name)
+
+    def _find(self, current_node, name):
+        if current_node is None:
+            return "Файл не знайдено"
+        if name == current_node.name:
+            return f"Файл: {current_node.name}, Розмір: {current_node.size} байт"
+        elif name < current_node.name:
+            return self._find(current_node.left, name)
+        else:
+            return self._find(current_node.right, name)
+
+    def delete_file(self, name):
+        self.root = self._delete(self.root, name)
+
+    def _delete(self, current_node, name):
+        if current_node is None:
+            return None
+        if name < current_node.name:
+            current_node.left = self._delete(current_node.left, name)
+        elif name > current_node.name:
+            current_node.right = self._delete(current_node.right, name)
+        else:
+            if current_node.left is None:
+                return current_node.right
+            if current_node.right is None:
+                return current_node.left
+            min_larger_node = self._get_min(current_node.right)
+            current_node.name, current_node.size = min_larger_node.name, min_larger_node.size
+            current_node.right = self._delete(current_node.right, min_larger_node.name)
+        return current_node
+
+    def _get_min(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node
+
+    def list_files(self):
+        files = []
+        self._in_order(self.root, files)
+        return files
+
+    def _in_order(self, current_node, files):
+        if current_node is not None:
+            self._in_order(current_node.left, files)
+            files.append((current_node.name, current_node.size))
+            self._in_order(current_node.right, files)
+
+tree = FileTree()
+tree.add_file("document.txt", 1024)
+tree.add_file("image.png", 2048)
+tree.add_file("video.mp4", 1048576)
+tree.add_file("audio.mp3", 5120)
+
+print(tree.find_file("image.png"))
+print(tree.find_file("unknown.txt"))
+
+tree.delete_file("image.png")
+print(tree.list_files())
